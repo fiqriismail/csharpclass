2c8952162919ac2082484a36fe8427c415b74f8d
Fiqri Ismail <fiqri.ismail@gmail.com>
2018-07-07 12:45:12 +0530

    Day 1 code

---
+++ Day1/Variables/CSharp/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        int number1 = 50;
+        static void Main(string[] args)
+        {
+            //Dont use too short names for variables 
+            //Dont use too long names for variables
+
+            int number1 = 20;
+            int number2 = 30;
+
+            string output = $"Adding {number1} + {number2} is {number1 + number2}";
+            Console.WriteLine(output);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
