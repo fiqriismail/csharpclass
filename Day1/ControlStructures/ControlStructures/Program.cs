c9cc7e20d069419227a1f5c4cd7ddaac3d4268f1
Fiqri Ismail <fiqri.ismail@gmail.com>
2018-07-07 15:24:58 +0530

    Day 1 - Control Structures

---
+++ Day1/ControlStructures/ControlStructures/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool printFirst3 = false;
+
+            if (printFirst3 == true)
+            {
+                Console.WriteLine(1);
+                Console.WriteLine(2);
+                Console.WriteLine(3);
+            }
+            else
+            {
+                Console.WriteLine(4);
+                Console.WriteLine(5);
+                Console.WriteLine(6);
+            }
+
+            // More ifs 
+            int passMarks = 85;
+            string output = passMarks >= 85 ? "PASS" : "FAIL";
+
+            Console.WriteLine(output);
+
+            bool isPass = passMarks > 60;
+
+            Console.WriteLine(isPass);
+
+            Console.ReadLine();
+        }
+    }
+}
